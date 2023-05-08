6f7c1a64d4b4e798457b7f572b82b27029ebb606
xiaweijiang <xiaweijiang@microsoft.com>
2023-05-08 08:41:02 +0000

    update

--- .hooks/pre_commit_hooks/check_conditional_mark_sort.py
+++ .hooks/pre_commit_hooks/check_conditional_mark_sort.py
@@ -18,7 +18,8 @@ def main():
                 sorted_conditions = conditions[:]
                 sorted_conditions.sort()
                 if conditions != sorted_conditions:
-                    print("line: {}".format(line))
+                    print("conditions: {}".format(conditions))
+                    print("sorted_conditions: {}".format(sorted_conditions))
                     print("The entries in tests/common/plugins/conditional_mark/tests_mark_conditions*.yaml "
                           "are not sorted in alphabetic order.")
                     retval = 1
